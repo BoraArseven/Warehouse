7a5bbaa717f7198b60c50ebfdd86a93165eeae51
Bora Arseven <bora.arseven@gmail.com>
2025-10-08 14:40:26 +0200

    Adjusting project structure

---
+++ airplanes/src/main/java/com/boracompany/airplanes/App.java
@@ -0,0 +1,18 @@
+package com.boracompany.airplanes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+
+    public int initialmethod() {
+        return 3;
+
+    }
+}
