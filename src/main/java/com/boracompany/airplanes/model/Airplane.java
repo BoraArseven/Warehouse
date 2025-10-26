4596eb782e37136d936095fe5e64c0bdea9544ac
Bora Arseven <bora.arseven@gmail.com>
2025-10-26 13:26:55 +0100

    added airplane model

--- src/main/java/com/boracompany/airplanes/model/Airplane.java
+++ src/main/java/com/boracompany/airplanes/model/Airplane.java
@@ -1,5 +1,59 @@
 package com.boracompany.airplanes.model;
 
-public class Airplane {
+import java.util.Objects;
 
+public class Airplane {
+    private String id;
+    private String model;
+
+    public Airplane() {
+
+    }
+
+    public Airplane(String id, String model) {
+        this.id = id;
+        this.model = model;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Airplane other = (Airplane) obj;
+        return Objects.equals(id, other.id) && Objects.equals(model, other.model);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Airplane  [id=" + id + ", name=" + model + "]";
+    }
 }
