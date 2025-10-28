d29f3e96ebb287c8449f1aa5e976c523bdf7ebfb
Bora Arseven <bora.arseven@gmail.com>
2025-10-28 13:53:41 +0100

    First test to controller, while view is not implemented, we mocked view and stubbed its method.

--- com.boracompany.airplanes/src/main/java/com/boracompany/airplanes/controller/WarehouseController.java
+++ com.boracompany.airplanes/src/main/java/com/boracompany/airplanes/controller/WarehouseController.java
@@ -1,16 +1,17 @@
 package com.boracompany.airplanes.controller;
 
 import com.boracompany.airplanes.repository.AirplaneRepository;
-import com.boracompany.airplanes.view.swing.AirplaneSwingView;
+import com.boracompany.airplanes.view.AirplaneView;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
 public class WarehouseController {
-    private AirplaneSwingView airplaneView;
+
+    private AirplaneView airplaneView;
     private AirplaneRepository airplaneRepository;
 
     @Inject
-    public WarehouseController(@Assisted AirplaneSwingView airplaneView, AirplaneRepository airplaneRepository) {
+    public WarehouseController(@Assisted AirplaneView airplaneView, AirplaneRepository airplaneRepository) {
         this.airplaneView = airplaneView;
         this.airplaneRepository = airplaneRepository;
     }
--- com.boracompany.airplanes/src/test/java/com/boracompany/airplanes/Controller/WarehouseControllerTest.java
+++ com.boracompany.airplanes/src/test/java/com/boracompany/airplanes/Controller/WarehouseControllerTest.java
@@ -1,24 +1,51 @@
 package com.boracompany.airplanes.Controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.boracompany.airplanes.controller.WarehouseController;
+import com.boracompany.airplanes.model.Airplane;
+import com.boracompany.airplanes.repository.AirplaneRepository;
+import com.boracompany.airplanes.view.AirplaneView;
 
 class WarehouseControllerTest {
 
+    @Mock
+    private AirplaneRepository airplaneRepository;
+    @Mock
+    private AirplaneView airplaneView;
+
+    @InjectMocks
+    private WarehouseController warehouseController;
+
+    private AutoCloseable closeable;
+
     @BeforeEach
     void setUp() throws Exception {
+        closeable = MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
     void tearDown() throws Exception {
+        closeable.close();
     }
 
     @Test
-    void testSchoolController() {
-        fail("Not yet implemented");
+    public void testAllAirplanes() {
+        List<Airplane> airplanes = Arrays.asList(new Airplane());
+        when(airplaneRepository.findAll()).thenReturn(airplanes);
+        warehouseController.allAirplanes();
+        verify(airplaneView).showAllAirplanes(airplanes);
     }
 
 }
